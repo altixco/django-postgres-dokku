a5bc814c48a47d88afda0d7addcb8237cfabdab6
Edison Arango <edisonarab@gmail.com>
2019-10-01 17:02:32 -0500

    configure url-loader and file-loader for imgs, fonts, and media

--- webpack.config.js
+++ webpack.config.js
@@ -77,6 +77,30 @@ module.exports = {
           }
         ],
       },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'img/[name].[hash:7].[ext]'
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'media/[name].[hash:7].[ext]'
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 10000,
+          name: 'fonts/[name].[hash:7].[ext]'
+        }
+      }
     ],
   },
 }
