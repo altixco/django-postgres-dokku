fb45896bdfe70f1846258c439f2499a5e3d8baa6
Francisco Rodríguez <franroca97@gmail.com>
2019-10-09 16:47:14 -0500

    added placeholder page

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,4 @@
 import "../scss/app.scss"
 
-async function component() {
-  return new Promise((res, rej) => {
-      setTimeout(() => {
-        console.log('Hello');
-        const element = document.createElement('div');
 
-        element.innerHTML = 'Hello async';
-
-        res(element);
-      }, 4000)
-  });
-}
-
-async function init() {
-  let el = await component();
-  document.body.appendChild(el);
-}
-
-init();
+console.log('Webpack working');
