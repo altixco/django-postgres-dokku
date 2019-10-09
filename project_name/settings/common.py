a5f9a54610e87c5dd02fdfde6f59833a7996f69b
Francisco Rodríguez <36459619+fraroca97@users.noreply.github.com>
2019-10-09 15:17:09 -0500

    Feature/rest config (#2)
    * initial commit
    
    * added accidentally removed config
    
    * minor changes
    
    * working

--- project_name/settings/common.py
+++ project_name/settings/common.py
@@ -91,6 +91,16 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    )
+}
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
