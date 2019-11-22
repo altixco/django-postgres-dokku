70fcc0d4f44cf017a6b29724c8c593b7b9e6ae2b
Edison Arango <edisonarab@gmail.com>
2019-11-21 20:39:20 -0500

    Enable localization by default

--- project_name/settings/common.py
+++ project_name/settings/common.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = DEPENDENCIES_APPS + PROJECT_APPS + ADDONS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
