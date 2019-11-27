820d414a1fb77ee0d18e092410922dfe9c8a30fe
Edison Arango <edisonarab@gmail.com>
2019-11-27 12:39:01 -0500

    serve statics when it's not production

--- project_name/urls.py
+++ project_name/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Uncomment to add main app urls to root page
     # path('', include('main.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if not settings.IS_PRODUCTION:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
